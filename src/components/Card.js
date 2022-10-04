b67a5ed0e25e94db7709ad571d67f73e3bfa9ca8
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-10-04 11:00:15 +0900

    fixed card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,25 @@
+import { FaMapMarkerAlt } from 'react-icons/fa';
+
 function Card({ title, location, googleMapsUrl, startDate, endDate, description, imageUrl }) {
   return (
-    <div className="card">
-      <div className="card-img">
-        <img src={imageUrl} alt="profile" />
-      </div>
-
-      <div className="card-info">
-        <div className="card-location">
-          {location}
-          <a href={googleMapsUrl}>View on Google Maps</a>
+    <div className="card-container">
+      <div className="card">
+        <div className="card-left">
+          <img src={imageUrl} className="card-img" alt="profile" />
         </div>
-        {title}
-        <h6>{startDate} - {endDate}</h6>
-        {description}
+
+        <div className="card-right">
+          <div className="card-location">
+            <div className="location-info">
+              <FaMapMarkerAlt className='location-icon' />
+              <p className="location-name">{location}</p>
+            </div>
+            <a href={googleMapsUrl}>View on Google Maps</a>
+          </div>
+          <h3 className="article-title">{title}</h3>
+          <p className="article-date"><span className="bold">{startDate} - {endDate}</span></p>
+          <p className="article-desc">{description}</p>
+        </div>
       </div>
     </div>
   );
