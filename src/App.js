88f36f8a84556e4b6294578163225bdd1549f580
Nikki Carlos <nikkirosecarlos@gmail.com>
2022-10-04 18:09:43 +0900

    finished travel journal project to demo map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import data from "./data";
 
 function App() {
+  const cards = data.map(card => {
+    return (
+      <Card
+        key={card.id}
+        title={card.title}
+        location={card.location}
+        googleMapsUrl={card.googleMapsUrl}
+        startDate={card.startDate}
+        endDate={card.endDate}
+        description={card.description}
+        imageUrl={card.imageUrl}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar />
-      <Card
-        title="Mount Fuji"
-        location="Japan"
-        googleMapsUrl="https://goo.gl/maps/1DGM5WrWnATgkSNB8"
-        startDate="12 Jan 2021"
-        endDate="24 Jan 2021"
-        description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-        imageUrl="https://source.unsplash.com/WLxQvbMyfas"
-      />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,25 @@
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
-function Card({ title, location, googleMapsUrl, startDate, endDate, description, imageUrl }) {
+function Card(props) {
+
   return (
     <div className="card-container">
       <div className="card">
         <div className="card-left">
-          <img src={imageUrl} className="card-img" alt="profile" />
+          <img src={props.imageUrl} className="card-img" alt="profile" />
         </div>
 
         <div className="card-right">
           <div className="card-location">
             <div className="location-info">
               <FaMapMarkerAlt className='location-icon' />
-              <p className="location-name">{location}</p>
+              <p className="location-name">{props.location}</p>
             </div>
-            <a href={googleMapsUrl}>View on Google Maps</a>
+            <a href={props.googleMapsUrl}>View on Google Maps</a>
           </div>
-          <h3 className="article-title">{title}</h3>
-          <p className="article-date"><span className="bold">{startDate} - {endDate}</span></p>
-          <p className="article-desc">{description}</p>
+          <h3 className="article-title">{props.title}</h3>
+          <p className="article-date"><span className="bold">{props.startDate} - {props.endDate}</span></p>
+          <p className="article-desc">{props.description}</p>
         </div>
       </div>
     </div>
